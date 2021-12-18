3385b7813127fefb8fd85766337b466878153515
aman-raza <amanraza1234@gmail.com>
2021-12-18 21:29:55 +0530

    4th commit

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 
 const Todo = () => {
+    const [inputdata, setInputData] = useState("")
+    const [items, setItems] = useState([])
+    // add item function
+    const addItem = () => {
+        if(!inputdata){
+            alert("Please enter a todo")
+        }
+        else{
+            const myNewInputData = {
+                id : new Date().getTime().toString(),
+                name : inputdata
+            }
+            setItems([...items, myNewInputData])
+            setInputData("")
+        }
+    }
+    // delete item function
+    const deleteItem = (index) => {
+        const updatedItems = items.filter((curElem) => {
+            return curElem.id !== index
+        })
+        setItems(updatedItems)
+    }
+
     return (
         <>
             <div className='main-div'>
@@ -11,18 +35,22 @@ const Todo = () => {
                         <figcaption>Add Your List Here 👀</figcaption>
                     </figure>
                     <div className='addItems'>
-                        <input className='form-control' type="text" placeholder= "😎 Add Item" />
-                        <i className="fa fa-plus add-btn"></i>
+                        <input className='form-control' type="text" placeholder= "😎 Add Item" value={ inputdata } onChange={ (event) => setInputData(event.target.value) } />
+                        <i className="fa fa-plus add-btn" onClick={addItem}></i>
                     </div>
                     {/* show our items */}
                     <div className='showItems'>
-                        <div className='eachItem'>
-                            <h3>Apple</h3>
-                            <div className='todo-btn'>
-                                <i className="far fa-edit add-btn"></i>
-                                <i className="far fa-trash-alt add-btn"></i>
-                            </div>
-                        </div>
+                        {items.map((curElem) => {
+                            return(
+                                <div className='eachItem' key={ curElem.id }>
+                                    <h3>{ curElem.name }</h3>
+                                    <div className='todo-btn'>
+                                        <i className="far fa-edit add-btn"></i>
+                                        <i className="far fa-trash-alt add-btn" onClick={ () => deleteItem(curElem.id) }></i>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
 
                     {/* remove all button */}

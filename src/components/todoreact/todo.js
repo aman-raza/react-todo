53a8150efa8e30a25b47bb77c9875ea409fb77b2
aman-raza <amanraza1234@gmail.com>
2021-12-18 19:00:24 +0530

    3rd commit

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -8,10 +8,26 @@ const Todo = () => {
                 <div className='child-div'>
                     <figure>
                         <img src="./images/todo.svg" alt='todo-logo' />
-                        <figcaption>Add Your List Here</figcaption>
+                        <figcaption>Add Your List Here 👀</figcaption>
                     </figure>
                     <div className='addItems'>
                         <input className='form-control' type="text" placeholder= "😎 Add Item" />
+                        <i className="fa fa-plus add-btn"></i>
+                    </div>
+                    {/* show our items */}
+                    <div className='showItems'>
+                        <div className='eachItem'>
+                            <h3>Apple</h3>
+                            <div className='todo-btn'>
+                                <i className="far fa-edit add-btn"></i>
+                                <i className="far fa-trash-alt add-btn"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* remove all button */}
+                    <div className='showItems'>
+                        <button className='btn effect04' data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
                     </div>
                 </div>
             </div>
